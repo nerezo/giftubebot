4b738b34a002764337aaabe41a70922006eb0b95
Okan Ozeren <Okan.Ozeren@telenity.com>
2016-06-20 15:18:09 +0300

    Improvements and bug fixes.

--- README.md
+++ README.md
@@ -60,7 +60,7 @@ Examples:
 /gift https://www.youtube.com/watch?v=QPFuwEqBgDQ 5
 ```
 
-If the second grather than 60 then crops only 60 seconds of the video.
+If the second grather than 30 then crops only 30 seconds of the video.
 Sample start times: 8, 1:23, 01:23, 1:23:45, 01:23:45. Durations can be used with dot charachters too like 1.23 or 02.34.
 Show Information: The video informations can be seen with "show-info" parameter with all other parameters as below.
 
--- giftubebot.js
+++ giftubebot.js
@@ -10,12 +10,14 @@ var fs = require('fs');
 
 var telegram_bot_key;
 try {
-  telegram_bot_key = fs.readFileSync('./telegram_bot_key.key', 'utf8');
+  telegram_bot_key = fs.readFileSync('./telegram_bot_key.key', 'utf8').trim();
   console.log('Telegram bot key has read.');
 } catch (e) {
   console.log('The telegram bot key file not found: "telegram_bot_key.key"');
   return;
 }
+
+var GENERIC_ERROR_MESSAGE = 'Error occured while converting the video! Please try again.\n/gifthelp';
 
 // The command to be use on telegram to run video cropping.
 var command = '/gift';
@@ -27,23 +29,8 @@ console.log('Telegram video to bot listener started...');
 
 fs.existsSync(utils.defaults.videoBowl) || fs.mkdirSync(utils.defaults.videoBowl);
 
-var startMessage = 'You can use below commands:\n\n' +
-  '/gift - crop videos from url with time parmaters\n' +
-  '/gifthelp - show description and usage information';
-
-var helpMessage = 'Crops specified part of YouTube or Vimeo videos and shows on the timeline.\n\n' +
-  'Usage: /gift <youtube_url> <start_time> <second>\n\n' +
-  'e.g.\n' +
-  'From the start time till the second:\n' +
-  '/gift https://www.youtube.com/watch?v=QPFuwEqBgDQ 00:00:04 5\n\n' +
-  'From the start time till the default second (5):\n' +
-  '/gift https://www.youtube.com/watch?v=QPFuwEqBgDQ 00:00:04\n\n' +
-  'From the beginning of the video till the second:\n' +
-  '/gift https://www.youtube.com/watch?v=QPFuwEqBgDQ 5\n\n' +
-  'If the second grather than 60 then crops only 60 seconds of the video.\n\n' +
-  'Sample start times: 8, 1:23, 01:23, 1:23:45, 01:23:45. Durations can be used with dot charachters too like 1.23 or 02.34.\n\n' +
-  'Show Information: The video informations can be seen with "show-info" parameter with all other parameters as below\n' +
-  '/gift https://www.youtube.com/watch?v=QPFuwEqBgDQ 00:00:04 5 show-info';
+var startText = fs.readFileSync('./texts/startTexts.txt', 'utf8');
+var helpText = fs.readFileSync('./texts/helpTexts.txt', 'utf8');
 
 // Help method to give usage information.
 tg.router.when(['/start'], 'GiftControllerStart')
@@ -58,8 +45,7 @@ tg.controller('GiftControllerStart', ($) => {
     var opts = {
       disable_web_page_preview: true
     };
-    //$.sendMessage(startMessage, opts);
-    tg.sendMessage($.chatId, startMessage, opts);
+    $.sendMessage(startText, opts);
   });
 
 });
@@ -70,7 +56,7 @@ tg.controller('GiftControllerHelp', ($) => {
   utils.logger.trace('GiftControllerHelp');
 
   tg.for('/gifthelp', ($) => {
-    var message = helpMessage + '\n\n' + startMessage;
+    var message = helpText + '\n\n' + startText;
 
     $.sendMessage(message);
   });
@@ -128,13 +114,17 @@ tg.controller('GiftControllerEdgeRange', ($) => {
 var giftController = function($, url, start, to, showInfo) {
   utils.logger.info('/gift called with the parameters url:', url, 'start:', start, 'to:', to, 'showInfo:', showInfo);
 
+  if (url.indexOf('www\.youtu\.be')) {
+    url = url.replace(/www\./g, '');
+  }
+
   try {
     start = utils.formatDurationString(start);
 
     utils.validateStartParam(start);
     utils.validateToParam(to);
   } catch (err) {
-    var message = err.message + ' Please try again.';
+    var message = err.message + ' Please try again.\ngifthelp';
     $.sendMessage(message);
 
     return;
@@ -147,7 +137,7 @@ var giftController = function($, url, start, to, showInfo) {
       // Checks values of times and normalizes.
       videoInfo.times = utils.normalizeDurations(videoInfo.duration, start, to);
     } catch (err) {
-      var message = err.message + ' Please try again.';
+      var message = err.message + ' Please try again.\ngifthelp';
       $.sendMessage(message);
 
       return;
@@ -161,11 +151,11 @@ var giftController = function($, url, start, to, showInfo) {
 
         // Crop a part of the video over the remote streaming url.
         ffmpegUtils.cropVideoFromUrl(videoInfo.times.start, videoInfo.times.to, resolution, realUrl, videoInfo.id).then(function(croppedFilename) {
-          utils.logger.log('Video successfully cropped: ', croppedFilename);
+          utils.logger.info('Video successfully cropped:', croppedFilename);
 
           // Embed logo to the cropped video.
-          ffmpegUtils.embedLogoAsWatermark(croppedFilename).then(function(finalFilename) {
-            utils.logger.log('Image successfully embedded to the video: ', finalFilename);
+          ffmpegUtils.embedLogoAsWatermark(croppedFilename, videoInfo.extractor).then(function(finalFilename) {
+            utils.logger.info('Image successfully embedded to the video:', finalFilename);
 
             // Create a stream from the downloaded video file.
             var finalFileWithPath = utils.defaults.videoBowl + '/' + finalFilename;
@@ -207,30 +197,37 @@ var giftController = function($, url, start, to, showInfo) {
           }, function(error) {
             utils.logger.error(error);
 
-            $.sendMessage('Error occured while converting the video! Please try again.');
+            $.sendMessage(GENERIC_ERROR_MESSAGE);
           });
         }, function(error) {
           utils.logger.error(error);
 
-          $.sendMessage('Error occured while converting the video! Please try again.');
+          $.sendMessage(GENERIC_ERROR_MESSAGE);
         });
       }, function(error) {
         utils.logger.error(error);
 
-        $.sendMessage('Error occured while converting the video! Please try again.');
+        $.sendMessage(GENERIC_ERROR_MESSAGE);
       });
     }, function(error) {
       utils.logger.error(error);
 
-      $.sendMessage('Error occured while converting the video! Please try again.');
+      var message = GENERIC_ERROR_MESSAGE;
+      if (String(error).indexOf('exist or is private' > -1)) {
+        message = 'YouTube said: The playlist doesn\'t exist or is private.';
+      }
+
+      $.sendMessage(message);
     });
   }, function(error) {
     utils.logger.error(error);
 
     if (String(error).indexOf('Please sign in') > -1) {
       message = 'YouTube said: This video requires authentication to view.'
+    } else if (String(error).indexOf('available') > -1) {
+      message = 'YouTube said: The uploader has not made this video available in your country.'
     } else {
-      message = 'The video does not exist! Check the url that you passed.';
+      message = 'The video does not exist! Check the url that you passed.\n/gifthelp';
     }
 
     $.sendMessage(message);
